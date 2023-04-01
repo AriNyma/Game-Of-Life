e997d13ab2f96be0206fcdf020aa240b9cc61ecf
Ari Nyman <e2203051@edu.vamk.fi>
2023-04-01 16:01:53 +0300

    template and struct added to c

---
+++ game_of_life.c
@@ -0,0 +1,88 @@
+/***************************************************************************
+ *   Copyright (C) 2023 by Ari Nyman                                       *
+ *   e2203051@edu.vamk.fi                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*********************************************************************
+
+ 1.  NAME
+        game_of_life
+
+ 2.  DESCRIPTION
+        
+
+ 3.  VERSIONS
+       Original:
+         1.4.2023 Arny
+
+       Version history:
+
+**********************************************************************/
+
+/*-------------------------------------------------------------------*
+*    HEADER FILES                                                    *
+*--------------------------------------------------------------------*/
+#include <stdio.h>
+
+/*-------------------------------------------------------------------*
+*    GLOBAL VARIABLES AND CONSTANTS                                  *
+*--------------------------------------------------------------------*/
+/* Control flags */
+
+/* Global constants */
+
+/* Global variables */
+
+/* Global structures */
+
+struct cell
+{
+    int current;    /*starting situation, shown on screen*/
+    int future;     /*upcomming situation*/
+}
+
+/*-------------------------------------------------------------------*
+*    FUNCTION PROTOTYPES                                             *
+*--------------------------------------------------------------------*/
+
+/*********************************************************************
+*    MAIN PROGRAM                                                      *
+**********************************************************************/
+
+int main(void)
+{
+
+} /* end of main */
+
+/*********************************************************************
+*    FUNCTIONS                                                       *
+**********************************************************************/
+
+
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME:
+ DESCRIPTION:
+	Input:
+	Output:
+  Used global variables:
+ REMARKS when using this function:
+*********************************************************************/
+
+
