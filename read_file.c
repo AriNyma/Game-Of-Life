67da92d282487fc2bfe0fdf3f0965e18007a9db6
Ari Nyman <e2203051@edu.vamk.fi>
2023-05-20 21:34:55 +0300

    Final

--- read_file.c
+++ read_file.c
@@ -25,75 +25,73 @@ void read_file(struct cell table[SIZE_COL][SIZE_ROW], char state_c[SIZE_COL])
         for(i = 0; i < SIZE_COL; i++)
         {
             for(j = 0; j < SIZE_ROW; j++) /*Write No File on the Map*/
-                {   
-                    /*N*/
-                    table[2][21].current = 1;
-                    table[3][21].current = 1;
-                    table[4][21].current = 1;
-                    table[5][21].current = 1;
-                    table[6][21].current = 1;
-                    table[2][22].current = 1;
-                    table[3][23].current = 1;
-                    table[4][24].current = 1;
-                    table[5][25].current = 1;
-                    table[6][26].current = 1;
-                    table[2][27].current = 1;
-                    table[3][27].current = 1;
-                    table[4][27].current = 1;
-                    table[5][27].current = 1;
-                    table[6][27].current = 1;
-                    /*O*/
-                    table[2][31].current_second = 1;
-                    table[2][30].current_second = 1;
-                    table[3][29].current_second = 1;
-                    table[3][32].current_second = 1;
-                    table[4][29].current_second = 1;
-                    table[4][32].current_second = 1;
-                    table[5][29].current_second = 1;
-                    table[5][32].current_second = 1;
-                    table[6][31].current_second = 1;
-                    table[6][30].current_second = 1;
-                    /*F*/
-                    table[12][2].current_second = 1;
-                    table[13][2].current_second = 1;
-                    table[14][2].current_second = 1;
-                    table[15][2].current_second = 1;
-                    table[16][2].current_second = 1;
-                    table[12][3].current_second = 1;
-                    table[12][4].current_second = 1;
-                    table[12][5].current_second = 1;
-                    table[14][3].current_second = 1;
-                    /*I*/
-                    table[12][7].current_second = 1;
-                    table[13][7].current_second = 1;
-                    table[14][7].current_second = 1;
-                    table[15][7].current_second = 1;
-                    table[16][7].current_second = 1;
-                    /*L*/
-                    table[12][9].current = 1;
-                    table[13][9].current = 1;
-                    table[14][9].current = 1;
-                    table[15][9].current = 1;
-                    table[16][9].current = 1;
-                    table[16][10].current = 1;
-                    table[16][11].current = 1;
-                    table[16][12].current = 1;
-                    /*E*/
-                    table[12][14].current = 1;
-                    table[13][14].current = 1;
-                    table[14][14].current = 1;
-                    table[15][14].current = 1;
-                    table[16][14].current = 1;
-                    table[12][15].current = 1;
-                    table[12][16].current = 1;
-                    table[12][17].current = 1;
-                    table[14][15].current = 1;
-                    table[16][15].current = 1;
-                    table[16][16].current = 1;
-                    table[16][17].current = 1;
-
-                    
-                }
+            {   
+                /*N*/
+                table[2][21].current = 1;
+                table[3][21].current = 1;
+                table[4][21].current = 1;
+                table[5][21].current = 1;
+                table[6][21].current = 1;
+                table[2][22].current = 1;
+                table[3][23].current = 1;
+                table[4][24].current = 1;
+                table[5][25].current = 1;
+                table[6][26].current = 1;
+                table[2][27].current = 1;
+                table[3][27].current = 1;
+                table[4][27].current = 1;
+                table[5][27].current = 1;
+                table[6][27].current = 1;
+                /*O*/
+                table[2][31].current_second = 1;
+                table[2][30].current_second = 1;
+                table[3][29].current_second = 1;
+                table[3][32].current_second = 1;
+                table[4][29].current_second = 1;
+                table[4][32].current_second = 1;
+                table[5][29].current_second = 1;
+                table[5][32].current_second = 1;
+                table[6][31].current_second = 1;
+                table[6][30].current_second = 1;
+                /*F*/
+                table[12][2].current_second = 1;
+                table[13][2].current_second = 1;
+                table[14][2].current_second = 1;
+                table[15][2].current_second = 1;
+                table[16][2].current_second = 1;
+                table[12][3].current_second = 1;
+                table[12][4].current_second = 1;
+                table[12][5].current_second = 1;
+                table[14][3].current_second = 1;
+                /*I*/
+                table[12][7].current_second = 1;
+                table[13][7].current_second = 1;
+                table[14][7].current_second = 1;
+                table[15][7].current_second = 1;
+                table[16][7].current_second = 1;
+                /*L*/
+                table[12][9].current = 1;
+                table[13][9].current = 1;
+                table[14][9].current = 1;
+                table[15][9].current = 1;
+                table[16][9].current = 1;
+                table[16][10].current = 1;
+                table[16][11].current = 1;
+                table[16][12].current = 1;
+                /*E*/
+                table[12][14].current = 1;
+                table[13][14].current = 1;
+                table[14][14].current = 1;
+                table[15][14].current = 1;
+                table[16][14].current = 1;
+                table[12][15].current = 1;
+                table[12][16].current = 1;
+                table[12][17].current = 1;
+                table[14][15].current = 1;
+                table[16][15].current = 1;
+                table[16][16].current = 1;
+                table[16][17].current = 1;  
+            }
         } 
     }
     while ((state = fgetc(fp)) != EOF)
